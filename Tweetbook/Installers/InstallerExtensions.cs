4a75c38ecbc37ae857c8b7f06e5b931780cb2497
MaxMartST <addergtx@gmail.com>
2021-08-19 23:18:27 +0300

    Создание ресурсов с помощью POST

--- Tweetbook/Installers/InstallerExtensions.cs
+++ Tweetbook/Installers/InstallerExtensions.cs
@@ -11,7 +11,9 @@ namespace Tweetbook.Installers
     {
         public static void InstallServicesInAssembly(this IServiceCollection services, IConfiguration configuration)
         {
-            var installers = typeof(Startup).Assembly.ExportedTypes.Where(x => typeof(IInstaller).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract).Select(Activator.CreateInstance).Cast<IInstaller>().ToList();
+            var installers = typeof(Startup).Assembly.ExportedTypes.Where(x => 
+                typeof(IInstaller).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract).Select(Activator.CreateInstance)
+            .Cast<IInstaller>().ToList();
 
             installers.ForEach(installer => installer.InstallServices(services, configuration));
         }
