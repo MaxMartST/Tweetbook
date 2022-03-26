8b532f116b9dbbb4c5e8f8432bfde0398269f84e
MaxMartST <AdderGtx@gmail.com>
2022-03-26 14:53:00 +0300

    Внес изменения

--- Tweetbook/Startup.cs
+++ Tweetbook/Startup.cs
@@ -36,6 +36,7 @@ namespace Tweetbook
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Внес изменения
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
